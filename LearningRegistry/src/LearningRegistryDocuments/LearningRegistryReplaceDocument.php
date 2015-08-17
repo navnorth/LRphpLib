9a57d18b9b2d7a633fd790618ae20978cc5c02b7
patlockley <patrick.lockley@googlemail.com>
2015-08-17 16:42:06 +0100

    LR node returns all arrays as objects added function to sort this

--- LearningRegistry/src/LearningRegistryDocuments/LearningRegistryReplaceDocument.php
+++ LearningRegistry/src/LearningRegistryDocuments/LearningRegistryReplaceDocument.php
@@ -11,6 +11,7 @@ class LearningRegistryReplaceDocument extends LearningRegistryDocument
       
     public function __construct($data)
     {
+    
         if (is_array($data)) {
             $LRConfig = new \LearningRegistry\LearningRegistryConfig(
                 array( "url" => $data[0])
@@ -21,20 +22,20 @@ class LearningRegistryReplaceDocument extends LearningRegistryDocument
             $LRObtain->ObtainService(
                 array(
                           "request_id" => $data[1],
-                          "by_doc_ID" => "true",
+                          "by_doc_ID" => "T",
                         )
             );
             $documentData = $LRObtain->getDocuments();
             $this->document = $documentData[0]->document[0];
             $this->id = $data[1];
-        
+            $this->ObjectToArray();
         }
         
     }
     
     public function emptyDocument($LR)
     {
-    
+   
         foreach ($this->document as $field => $fieldValue) {
             switch($field){
                 case "doc_type": $LR->setResFields(array($field => $fieldValue));
@@ -46,11 +47,6 @@ class LearningRegistryReplaceDocument extends LearningRegistryDocument
                 case "keys": $LR->setResFields(array("keys" => ""));
                     break;
                 case "TOS": $LR->setTosFields($fieldValue);
-                    break;
-                case "digital_signature": foreach ($fieldValue as $id => $value) {
-                          $fieldValue->{$id} = "";
-                }
-                $LR->setSigFields($fieldValue);
                     break;
                 case "resource_data_type": $LR->setResFields(array($field => $fieldValue));
                     break;
@@ -64,7 +60,8 @@ class LearningRegistryReplaceDocument extends LearningRegistryDocument
                     break;
                 case "identity": $LR->setIdFields($fieldValue);
                     break;
-                break;
+                default: unset($this->document->{$field});
+                    break;
             }
         }
         $LR->setResFields(array("replaces" => array($this->id)));
